30e71605eb9c18b2645172f8221fc11a8a719ac9
Daniel Steman <daniel-steman@live.nl>
2025-11-03 20:28:19 +0100

    fix: kitty scripts

--- scripts/start-dev-kitty.sh
+++ scripts/start-dev-kitty.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-kitty --session "$SCRIPT_DIR/dev.kitty-session"
+kitty --single-instance --session "$SCRIPT_DIR/dev.kitty-session"
